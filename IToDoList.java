a66ec8cc5458c7930abb37feda6bd4c5ea0e1cf3
Aaron Lauang <aaron.lauang1@ucalgary.ca>
2025-03-03 11:37:59 -0700

    Delete IToDoList.java

--- IToDoList.java
+++
@@ -1,8 +0,0 @@
-public interface IToDoList {
-    void addTask(Task newTask);
-    void completeTask(String taskNum);
-    void deleteTask(String taskNum);
-    void editTask(String taskNum, String task, boolean bool);
-    void undo();
-    void listTasks();
-}
