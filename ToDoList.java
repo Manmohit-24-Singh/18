356d8c8f2d292ebe39b959ccef71ed3b5306aea9
Sabrina <144979434+realvegetablegummy@users.noreply.github.com>
2025-03-03 11:28:21 -0700

    added ToDoList class

---
+++ ToDoList.java
@@ -0,0 +1,26 @@
+/**
+ * @author      Sabrina Li <a href="mailto:sabrina.li1@ucalgary.ca">sabrina.li1@ucalgary.ca</a>
+ * @version     1.0
+ * @since       1.0
+ */
+
+ import java.util.*;
+
+ public class ToDoList implements IToDoList {
+    public void addTask(Task taskToAdd) {}
+
+    public void completeTask(String id) {}
+
+    public void deleteTask(String taskToDelete) {}
+
+    public void editTask(String id, String title, boolean isCompleted) {}
+
+
+
+    public List<Task> listTasks() {
+    }
+
+    public void undo() {
+    }
+
+ }
