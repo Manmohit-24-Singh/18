53ee6c1bbca7d74b2210de88cec66a36f966143e
Sabrina <144979434+realvegetablegummy@users.noreply.github.com>
2025-03-03 11:58:38 -0700

    need edit, undo

--- edu/ucalgary/oop/ToDoList.java
+++ edu/ucalgary/oop/ToDoList.java
@@ -2,27 +2,55 @@
  * @author      Sabrina Li <a href="mailto:sabrina.li1@ucalgary.ca">sabrina.li1@ucalgary.ca</a>
  * @version     1.0
  * @since       1.0
- */
+*/
 
- package edu.ucalgary.oop;
+package edu.ucalgary.oop;
 
- import java.util.*;
+import java.util.*;
 
- public class ToDoList implements IToDoList {
-    public void addTask(Task taskToAdd) {}
+public class ToDoList implements IToDoList {
+    private List<Task> toDoList;
+    private Stack<List<Task>> listHistory;
 
-    public void completeTask(String id) {}
+    
+    public void addTask(Task taskToAdd) {
+        updateListHistory();
 
-    public void deleteTask(String taskToDelete) {}
+        this.toDoList.add(taskToAdd);
+    }
+
+    public void completeTask(String id) {
+        updateListHistory();
+
+        Iterator<Task> iter = this.toDoList.iterator();
+
+        while (iter.hasNext()) {
+            if (iter.next().getId() == id) {
+                iter.next().setIsCompleted(true);
+            }
+        }
+    }
+
+    public void deleteTask(String taskToDelete) {
+        updateListHistory();
+
+        this.toDoList.remove(taskToDelete);
+    }
 
     public void editTask(String id, String title, boolean isCompleted) {}
-
+    // need updateListHistory(), edit stuff
 
 
     public List<Task> listTasks() {
+        return this.toDoList;
+    }
+
+    public void updateListHistory() {
+        List<Task> currentList = this.listTasks();
+        this.listHistory.push(currentList);
     }
 
     public void undo() {
     }
 
- }
+}
